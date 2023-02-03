66f357d422052432edfd5c970dbd464d73013a41
James Likier <james.likier@gmail.com>
2023-02-03 14:12:27 -0600

    working on stack

--- datastructures/datastructures/Stack.cs
+++ datastructures/datastructures/Stack.cs
@@ -1,9 +1,38 @@
 ﻿namespace datastructures
 {
-    public class Stack
+    public class Stack<T>
     {
+        private Node? root;
+        private int _length;
+        public int length
+        {
+            get
+            {
+                return _length;
+            }
+        }
+
+        private class Node {
+            public Node? prev;
+            public T? data;
+            public Node(T? data, Node? prev)
+            {
+                this.data = data;
+                this.prev = prev;
+            }
+        }
         public Stack()
         {
+            _length = 0;
+            root = null;
+        }
+
+        public void push(T data)
+        {
+            if (root == null)
+            {
+                //todo
+            }
         }
     }
 }
