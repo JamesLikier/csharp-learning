597c8495c4cef24f65be4660e605a65fcc52059e
James Likier <james.likier@gmail.com>
2023-02-04 16:52:07 -0600

    Added Queue. Added testing for Queue.

---
+++ datastructures/datastructures-test/QueueTest.cs
@@ -0,0 +1,44 @@
+﻿using ds = datastructures;
+
+namespace datastructures_test
+{
+    [TestClass]
+    public class QueueTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            ds.Queue<int> q = new();
+            Assert.AreEqual(0, q.Count);
+            q.Add(1);
+            Assert.AreEqual(1, q.Count);
+            q.Add(1);
+            Assert.AreEqual(2, q.Count);
+            q.Remove();
+            q.Remove();
+            q.Add(1);
+            Assert.AreEqual(1, q.Count);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            ds.Queue<int> q = new();
+            Assert.ThrowsException<Exception>(() => { q.Remove(); });
+            q.Add(1);
+            q.Add(2);
+            q.Add(3);
+            Assert.AreEqual(3, q.Count);
+            Assert.AreEqual(1, q.Remove());
+            Assert.AreEqual(2, q.Count);
+            q.Add(4);
+            Assert.AreEqual(2, q.Remove());
+            Assert.AreEqual(2, q.Count);
+            Assert.AreEqual(3, q.Remove());
+            Assert.AreEqual(1, q.Count);
+            q.Remove();
+            Assert.AreEqual(0, q.Count);
+            Assert.ThrowsException<Exception>(() => { q.Remove(); });
+        }
+    }
+}
---
+++ datastructures/datastructures/Queue.cs
@@ -0,0 +1,47 @@
+﻿namespace datastructures
+{
+    public class Queue<T>
+    {
+        private int _count;
+        public int Count { get { return _count; } }
+
+        private class Node
+        {
+            public Node? Next;
+            public T Data;
+
+            public Node(T data, Node? next)
+            {
+                this.Data = data;
+                this.Next= next;
+            }
+        }
+        private Node? Root;
+        private Node? Last;
+        public Queue()
+        {
+            Root = null;
+            Last = null;
+            _count= 0;
+        }
+
+        public void Add(T data)
+        {
+            Node n = new(data, null);
+            if (Last is not null) Last.Next = n;
+            Root ??= n;
+            Last = n;
+            _count++;
+        }
+
+        public T Remove()
+        {
+            if (Root is null) throw new Exception("Queue is Empty");
+            Node n = Root;
+            Root = Root.Next;
+            n.Next= null;
+            _count--;
+            return n.Data;
+        }
+    }
+}
