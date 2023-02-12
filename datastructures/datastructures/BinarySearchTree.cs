18e40ffd41627131ac29da063b7993bb176bfb15
James Likier <james.likier@gmail.com>
2023-02-12 05:24:34 -0600

    Added Add, Remove, Contains, and comparer to BinarySearchTree.

--- datastructures/datastructures/BinarySearchTree.cs
+++ datastructures/datastructures/BinarySearchTree.cs
@@ -22,11 +22,13 @@ namespace datastructures
         private Node? Root;
         private int _count;
         public int Count { get { return _count; } }
+        private Comparer<T> _comparer;
 
-        public BinarySearchTree()
+        public BinarySearchTree(Comparer<T> comparer)
         {
             this.Root = null;
             this._count = 0;
+            this._comparer = comparer;
         }
 
         public void Add(T data)
@@ -39,25 +41,150 @@ namespace datastructures
             //not empty
             else
             {
-                Node? n = Root;
-                if(data < n.data && n.children[Node.LEFT] is null)
+                Node? current = Root;
+                Node? parent = null;
+                while (true)
                 {
-                    n.children[Node.LEFT] = new Node(data, n, null, null);
+                    parent = current;
+                    int weight = this._comparer.Compare(data, parent.data);
+                    //duplicate already inserted
+                    if (weight == 0) return;
+                    //go left
+                    if (weight < 0)
+                    {
+                        current = current.children[Node.LEFT];
+                        //insert left
+                        if (current is null)
+                        {
+                            parent.children[Node.LEFT] = new Node(data, parent, null, null);
+                            return;
+                        }
+                    }
+                    //go right
+                    else
+                    {
+                        current = current.children[Node.RIGHT];
+                        //insert right
+                        if (current is null)
+                        {
+                            parent.children[Node.RIGHT] = new Node(data, parent, null, null);
+                            return;
+                        }
+                    }
                 }
-                else if (data > n.data && n.children[Node.RIGHT] is null)
+            }
+        }
+        private Node? FindNode(T data)
+        {
+            if (Root is null) return null;
+            Node? current = Root;
+            while (true)
+            {
+                int weight = this._comparer.Compare(data, current.data);
+                //found node
+                if (weight == 0) return current;
+                //go left
+                if (weight < 0)
                 {
-                    n.children[Node.RIGHT] = new Node(data, n, null, null);
+                    current = current.children[Node.LEFT];
+                    if (current is null) return null;
                 }
-                //todo
+                //go right
+                else
+                {
+                    current = current.children[Node.RIGHT];
+                    if (current is null) return null;
+                }
             }
         }
 
         public bool Remove(T data)
         {
+            Node? n = FindNode(data);
+            //didn't find data
+            if (n is null) return false;
+            Node? parent = n.parent;
+            Node? rChild = n.children[Node.RIGHT];
+            Node? lChild = n.children[Node.LEFT];
+            //no children
+            if (lChild is null && rChild is null)
+            {
+                this.RemoveNoChild(n);
+            }
+            //one child 
+            else if (lChild is not null ^ rChild is not null)
+            {
+                this.RemoveOneChild(n);
+            }
+            //two children
+            else
+            {
+                //find in-order successor
+                Node? current = rChild;
+                while (current is not null && current.children[Node.LEFT] is not null) current = current.children[Node.LEFT];
+                n.data = current!.data;
+                Node? cChild = current.children[Node.LEFT] ?? current.children[Node.RIGHT];
+                if(cChild is null)
+                {
+                    RemoveNoChild(current);
+                }
+                else
+                {
+                    RemoveOneChild(current);
+                }
+            }
+            return true;
+        }
+        private void RemoveNoChild(Node n)
+        {
+            Node? parent = n.parent;
+            //root node
+            if (parent is null)
+            {
+                this.Root = null;
+            }
+            else
+            {
+                int weight = this._comparer.Compare(n.data, parent.data);
+                //chop parent left child
+                if (weight < 0)
+                {
+                    parent.children[Node.LEFT] = null;
+                }
+                //chop parent right child
+                else
+                {
+                    parent.children[Node.RIGHT] = null;
+                }
+            }
+        }
+        private void RemoveOneChild(Node n)
+        {
+            Node? parent = n.parent;
+            Node child = n.children[Node.LEFT] ?? n.children[Node.RIGHT]!;
+            if (parent is null)
+            {
+                this.Root = child;
+                this.Root.parent = null;
+            }
+            else
+            {
+                int weight = this._comparer.Compare(n.data, parent.data);
+                if (weight < 0)
+                {
+                    parent.children[Node.LEFT] = child;
+                }
+                else
+                {
+                    parent.children[Node.RIGHT] = child;
+                }
+                child.parent = parent;
+            }
         }
 
         public bool Contains(T data)
         {
+            return FindNode(data) is not null;
         }
     }
 }
