124ec27c7e4b770958ec2e27b9900649adf3dd23
James Likier <james.likier@gmail.com>
2023-02-18 15:27:30 -0600

    Added testing for BinarySearchTree: .PreOrder(),.BreadthFirst(),.InOrder().

--- datastructures/datastructures-test/BinarySearchTreeTest.cs
+++ datastructures/datastructures-test/BinarySearchTreeTest.cs
@@ -10,6 +10,8 @@
     {
         public static int[] nums = new int[] { 10, 5, 3, 4, 2, 1, 15, 17, 16, 20, 19, 18, 6, 7, 14 };
         public static ds.ArrayList<int> numsPreOrder = new() { 10, 5, 3, 2, 1, 4, 6, 7, 15, 14, 17, 16, 20, 19, 18 };
+        public static ds.ArrayList<int> numsBreadthFirst = new() { 10, 5, 15, 3, 6, 14, 17, 2, 4, 7, 16, 20, 1, 19, 18 };
+        public static ds.ArrayList<int> numsInOrder = new() { 1, 2, 3, 4, 5, 6, 7, 10, 14, 15, 16, 17, 18, 19, 20 };
         /*
          *                      10
          *                 5          15
@@ -53,6 +55,24 @@
 
             Assert.AreEqual(nums.Length-5, bst.Count);
             Assert.IsTrue(preorder is [14, 5, 4, 2, 6, 15, 17, 16, 20, 18]);
+        }
+        [TestMethod]
+        public void BreadthFirstTest()
+        {
+            BinarySearchTree<int> bst = CreateTree();
+            Assert.IsTrue(bst.BreadthFirst().ToArray().SequenceEqual(numsBreadthFirst));
+        }
+        [TestMethod]
+        public void PreOrderTest()
+        {
+            BinarySearchTree<int> bst = CreateTree();
+            Assert.IsTrue(bst.PreOrder().ToArray().SequenceEqual(numsPreOrder));
+        }
+        [TestMethod]
+        public void InOrderTest()
+        {
+            BinarySearchTree<int> bst = CreateTree();
+            Assert.IsTrue(bst.InOrder().ToArray().SequenceEqual(numsInOrder));
         }
     }
 }
